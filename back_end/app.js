2adcf279c1284910b1d33d577e7b6f26d3392ca0
Sakura Kano <kanosakura@icloud.com>
2023-12-02 23:29:09 +0800

    working on sign up and login

--- back_end/app.js
+++ back_end/app.js
@@ -21,47 +21,33 @@ const conn = mysql.createConnection({
     res.send('hello')
   })
 
-  //////////login&authentication/////////
+  //////////sign up
 
-  app.post('/login', (req,res)=>{
-    var user = conn.query.find((x)=> x.email == req.body.email && x.password == req.body.password);
-
-    if(user == null){
-      return res.send({message: "user does not exist"});
-    }else{
-      if(user.password == req.body.password){
-        const secretKey = crypto.randomBytes(32).toString('hex');
-
-        var token = jwt.sign(user,secretKey, {expiresIn: '1h'});
-      }else{
-        return res.send({message: "wrong password"});
-      }
+  app.post('/customersSignUp', (req, res) => {
+    // Input validation - Check for required fields
+    if (!req.body.username || !req.body.email || !req.body.mobile_number || !req.body.password) {
+        return res.status(400).json({ error: 'All fields are required' });
     }
-  })
 
-  //MIDDLEWARE//
-  const authenticateToken = (req,res,next)=>{
-    const authHeader = request.headers["authorization"];
-    console.log(authHeader);
+    // Insert data into the database
+    conn.query("INSERT INTO customer(username, email, mobile_number, password, role) VALUES (?,?,?,?,?)", 
+        [req.body.username, req.body.email, req.body.mobile_number, req.body.password, 0], 
+        (error, data) => {
+            if (error) {
+                console.error(error);
+                return res.status(500).json({ error: 'Internal Server Error' });
+            }
 
-    var token = null;
-    if(authHeader != undefined && authHeader != null){
-      token = authHeader.split(" ")[1]
-    }else{
-      jwt.verify(token, secretKey, (error, res)=>{
-        if(error){
-          res.send({message: "unauthorized token"});
-          return;
+            console.log(data);
+            res.status(201).json({ success: true, data });
         }
-        req.user = res;
-      })
-      next();
-    }
-  }
+    );
+});
 
-  app.post('')
 
-  app.get('/foods', (req, res)=>{
+  ////getting the list of foods is available to everyone
+
+  app.get('/getFoods', (req, res)=>{
     conn.query("SELECT * FROM food", (error, data)=>{
         if(error==null){
             res.send(data)
@@ -71,16 +57,74 @@ const conn = mysql.createConnection({
     })
   })
 
-  app.post('/customers', (req,res)=>{
-    conn.query("INSERT INTO customer( last_name, first_name, email, mobile_number, password) VALUES (?,?,?,?,?)", [req.body.last_name, req.body.first_name, req.body.email, req.body.mobile_number, req.body.password], (error, data)=>{
-        if(req.body.last_name!= undefined && req.body.first_name != undefined && req.body.email != undefined && req.body.mobile_number != undefined && req.body.password != undefined){
-            console.log(req.body)
-            res.send(data)
-        }else{
-            res.send(error)
+
+
+  //////////login&authentication/////////
+
+   app.post('/login', (req, res) => {
+    conn.query("SELECT * FROM customer WHERE email = ? AND password = ?", [req.body.email, req.body.password], (error, results) => {
+      if (error) {
+          console.error(error);
+          return res.status(500).json({ message: 'Internal Server Error' });
+      }
+
+      if (results.length === 0) {
+          return res.status(401).json({ message: 'User does not exist or wrong credentials' });
+      }
+
+      const user = results[0];
+
+      // Now you have the user data, and you can proceed with further checks or actions.
+      const secretKey = 'ifhvseiguehrifvuejsrfiu';
+      const token = jwt.sign({ id: user.id, email: user.email }, secretKey, { expiresIn: '1h' });
+
+      res.status(200).json({ message: 'Login successful', token });
+  });
+});
+
+
+  //MIDDLEWARE//
+  const authToken = (req, res, next) => {
+    const authHeader = req.headers["authorization"];
+
+    if (!authHeader) {
+        return res.status(401).json({ message: "Unauthorized: Missing token" });
+    }
+
+    const token = authHeader.split(" ")[1];
+    console.log(token);
+
+    jwt.verify(token, 'ifhvseiguehrifvuejsrfiu', (error, decoded) => {
+        if (error) {
+            console.error(error);
+            return res.status(401).json({ message: "Unauthorized token" });
         }
-    })
+
+        // Assign the decoded user information to the request object
+        req.user = decoded;
+
+        // Call the next middleware in the stack
+        next();
+    });
+};
+
+  
+
+  ////posting foods is only allowed by admins
+  app.post('/postFood', authToken,(req,res)=>{
+    if(req.user.role == 1){
+    conn.query("INSERT INTO food(food_name, description, price, quantity) VALUES (?,?,?,?)", [req.body.food_name, req.body.description, req.body.price, req.body.quantity], (error, data)=>{
+      if(req.body.food_name != undefined && req.body.description != undefined && req.body.price && req.body.quantity){
+        
+        console.log(req.body)
+        res.send(data)
+        }
+        })
+      }else{
+        res.send({message: "has no authorization"})
+      }
   })
+
 
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
